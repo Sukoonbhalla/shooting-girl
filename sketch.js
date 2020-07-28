0863191b4d6d4732816851d00e7ab75cb23fa715
Sukoonbhalla <sukoonbhalla@gmail.com>
2020-07-28 19:06:32 +0530

    s

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+var ground;
+var girl,girlImg;
+
+function preload(){
+  girlImg=loadImage("images/download.png");
+  
+
+}
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400,400);
+ girl =  createSprite(120,265,20,50);
+ girl.addAnimation("girl1",girlImg);
+ girl.scale = 3.5;
+
+  ground = createSprite(200,380,400,10);
+
+  ground.shapeColor = "red";
+  ground.x = width/2;
+  ground.velocityX = -3; 
 }
 
 function draw() {
-  background(255,255,255);  
+ 
+ 
+  background(0); 
+  if(ground.x<0){
+   ground.x = ground.width/2 
+  }
+  spawnObject();
   drawSprites();
+}
+function spawnObject(){
+  if(frameCount % 60===0){
+  var targets = createSprite(200,200,20,20);
+  targets.velocityX = -2;
+  }
 }
